a3e985d7cacd4baa5a1d71aa0c504ef7ddd982be
Tischlinger Alex <32167982+Alexikon@users.noreply.github.com>
2020-02-10 10:09:56 +0100

    copied specification with GitDown

---
+++ Solution from Stropek copied from GitDown website/CashRegister/CashRegister.UI/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace CashRegister.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel ViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Set the data context for data binding
+            DataContext = ViewModel = new MainWindowViewModel();
+
+            // When the view has been loaded, give the view model
+            // a chance to initialize.
+            Loaded += async (_, __) => await ViewModel.InitAsync();
+        }
+    }
+}
